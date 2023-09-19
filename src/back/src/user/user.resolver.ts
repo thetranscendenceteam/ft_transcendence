afa19dc0431dba8cab8406f44e6d4896b5de20ef
Yukie <alain.huber91@gmail.com>
2023-09-19 14:11:15 +0200

    graphql_init still in progress

---
+++ src/back/src/user/user.resolver.ts
@@ -0,0 +1,33 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/createUser.input';
+import { AddXp } from './dto/addXp.input';
+
+@Resolver(of => User)
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Query(returns => [User])
+  user(): Promise<User[]> {
+    return this.userService.getAllUser();
+  }
+
+  @Mutation(returns => User)
+  createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    if (!createUserInput.avatar) createUserInput.avatar = 'Default';
+    return this.userService.createUser(createUserInput);
+  }
+
+  @Mutation(returns => User)
+  addXpByNickname(@Args('addXp') addXp: AddXp): Promise<User> {
+    return this.userService.addXpByNickname(addXp);
+  }
+
+  @Mutation(returns => User)
+  initMockDB(): Promise<User> {
+    return this.userService.initMockDB();
+  }
+}

0a9603882083343aaf371530679740722d144561
Fargesia01 <87009346+Fargesia01@users.noreply.github.com>
2024-02-02 15:48:02 +0100

    Front score board (#15)
    * strucure_done
    
    * tmp
    
    * updated_score_board
    
    * scoreboard_finished
    
    * scoreboard_fetch_data

--- src/back/src/main.ts
+++ src/back/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors()); // Use the cors middleware
   await app.listen(3000);
 }
 bootstrap();

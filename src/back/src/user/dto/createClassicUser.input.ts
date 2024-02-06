7330cfffcbb67722374cae20b80d43a7c5e5e310
Alain Huber <Huberal@MAC1064.groupemutuel.ch>
2024-02-06 22:16:06 +0100

    wip

--- src/back/src/user/dto/createClassicUser.input.ts
+++ src/back/src/user/dto/createClassicUser.input.ts
@@ -2,21 +2,21 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateClassicUserInput {
-  @Field(() => String)
-  mail: string;
+    @Field(() => String)
+    mail: string;
 
-  @Field(() => String)
-  password: string;
+    @Field(() => String)
+    password: string;
 
-  @Field(() => String)
-  firstName: string;
+    @Field(() => String)
+    firstName: string;
 
-  @Field(() => String)
-  lastName: string;
+    @Field(() => String)
+    lastName: string;
 
-  @Field(() => String, { nullable: true })
-  avatar: string | null;
+    @Field(() => String, { nullable: true })
+    avatar: string | null;
 
-  @Field(() => String)
-  pseudo: string;
+    @Field(() => String)
+    pseudo: string;
 }

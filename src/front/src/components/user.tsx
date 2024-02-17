d0b041bdedd0b79666d7b8c04cf78c9ee1fa0949
Fargesia01 <slott@student.42lausanne.ch>
2024-02-17 21:05:23 +0100

    not_work

--- src/front/src/components/user.tsx
+++ src/front/src/components/user.tsx
@@ -75,6 +75,9 @@ const UserComponent = (props: {username: string}) => {
   const [matchHistory, setMatchHistory] = useState<MatchHistory[]>();
   const { user } = useContext(UserContext);
   const [relationship, setRelationship] = useState<string>();
+  const [user1, setUser1] = useState<string>();
+  const [user2, setUser2] = useState<string>();
+  const [status, setStatus] = useState<string>();
   const [refresh, setRefresh] = useState<boolean>(false);
   const username = props.username;
 
@@ -83,7 +86,11 @@ const UserComponent = (props: {username: string}) => {
       const { data } = await apolloClient.query({
         query: gql`
           query findRelationshipBetweenUsers($input: RelationshipInput!) {
-            findRelationshipBetweenUsers(relationshipInput: $input)
+            findRelationshipBetweenUsers(relationshipInput: $input) {
+              user1
+              user2
+              status
+            }
           }
         `,
         variables: {
@@ -159,6 +166,27 @@ const UserComponent = (props: {username: string}) => {
     }
   }
 
+  const changePending = async(userId: string, targetId: string, accept: boolean) => {
+    try {
+      const { data } = await apolloClient.mutate({
+        mutation: gql`
+          mutation acceptOrRefusePending($input: acceptOrNot, relationshipInput) {
+            acceptOrRefusePending(acceptOrNot, relationshipInput)
+          }
+        `,
+        variables: {
+          input: {
+            userId: userId,
+            targetId: targetId,
+            acceptOrNot: accept
+          }
+        }
+      });
+    } catch (error) {
+      return ([]);
+    }
+  }
+
   useEffect(() => {
     const fetchUser = async (username: string) => {
       try {
@@ -187,7 +215,9 @@ const UserComponent = (props: {username: string}) => {
       try {
         const rel = await getRelationship(userId, targetId);
         console.log("RELATION : ", rel);
-        setRelationship(rel);
+        setRelationship(rel.status);
+        setUser1(rel.user1);
+        setUser2(rel.user2);
       } catch (error) {
         throw(error);
       }
@@ -212,15 +242,40 @@ const UserComponent = (props: {username: string}) => {
     fetchMatchHistory();
   }, [user]);
 
-  const unfriend = () => {
-    if (user && target) {
-      removeRelationship(user.id, target.id);
-      setRefresh(!refresh);
+  useEffect(() => {
+    console.log("USER 1 : ", user1);
+    if (user)
+      console.log("CURRENT USER : ", user.id);
+    console.log("RELAFUCK : ", relationship);
+    if (relationship === 'friends') {
+      setStatus('friends');
+    } else if (!relationship || relationship === 'unknown') {
+      setStatus('unknown');
+    } else if (user && relationship === 'block_first_to_second' && user.id === user1) {
+      setStatus('has_blocked');
+    } else if (user && relationship === 'block_second_to_first' && user.id === user2) {
+      setStatus('has_blocked');
+    } else if (user && relationship === 'block_first_to_second' && user.id === user2) {
+      setStatus('was_blocked');
+    } else if (user && relationship === 'block_second_to_first' && user.id === user1) {
+      console.log("TEST");
+      setStatus('was_blocked');
+      console.log("DINGUERIE", status)
+    } else if (user && relationship === 'pending_first_to_second' && user.id === user1) {
+      setStatus('has_invited');
+    } else if (user && relationship === 'pending_second_to_first' && user.id === user2) {
+      setStatus('has_invited');
+    } else if (user && relationship === 'pending_first_to_second' && user.id === user2) {
+      setStatus('was_invited');
+    } else if (user && relationship === 'pending_second_to_first' && user.id === user1) {
+      setStatus('was_invited');
     }
-  }
+    console.log("STATUS : ", status);
+  }, [relationship]);
 
   const addFriend = () => {
     if (user && target) {
+      removeRelationship(user.id, target.id);
       mutateAddFriend(user.id, target.id);
       setRefresh(!refresh);
     }
@@ -233,22 +288,44 @@ const UserComponent = (props: {username: string}) => {
       setRefresh(!refresh);
     }
   }
+
+  const removeRelation = () => {
+    if (user && target) {
+      removeRelationship(user.id, target.id);
+      setRefresh(!refresh);
+    }
+  }
   
-  if (target) {
+  if (target && status != "was_blocked") {
     return (
       (target && matchHistory) && (
         <div className={styles.container}>
           <div className="w-1/4 flex flex-col">
             <UserProfileCard user={target} matchHistory={matchHistory} />
-            {relationship === 'friends' && (
+            {status === 'friends' && (
               <>
-                <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={unfriend}>Unfriend</button>
+                <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={removeRelation}>Unfriend</button>
                 <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={block}>Block</button>
               </>
             )}
-            {relationship === 'unknown' && (
+            {status === 'unknown' && (
               <>
                 <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={addFriend}>Add Friend</button>
+                <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={block}>Block</button>
+              </>
+            )}
+            {status === 'has_blocked' && (
+              <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={removeRelation}>Unblock</button>
+            )}
+            {status === 'has_invited' && (
+              <>
+                <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={removeRelation}>Cancel Invite</button>
+                <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={block}>Block</button>
+              </>
+            )}
+            {status === 'was_invited' && (
+              <>
+                <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80">Accept invitation</button>
                 <button className="h-24 bg-blue-600 hover:bg-blue-500 ml-6 mt-6 rounded-xl w-80" onClick={block}>Block</button>
               </>
             )}
@@ -257,6 +334,8 @@ const UserComponent = (props: {username: string}) => {
         </div>
       )
     );
+  } else if (target && status == "was_blocked") {
+    <p className="text-10xl">YOU WERE BLOKED FUCKER KEK</p>
   }
   return (
     <div className={styles.container}>

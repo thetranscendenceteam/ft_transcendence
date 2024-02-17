2ff9d518cc232b9846cc280dfb4778388406072d
yukiech <87153011+yukiech@users.noreply.github.com>
2024-02-17 11:22:24 +0100

    Fix register window closure (#66)
    Co-authored-by: Alain Huber <Huberal@MAC1064.groupemutuel.ch>

--- src/front/src/components/register-dialog.tsx
+++ src/front/src/components/register-dialog.tsx
@@ -15,7 +15,7 @@ import { Label } from "@/components/ui/label"
 import apolloClient from "./apolloclient";
 import { gql } from "@apollo/client";
 import { useState } from "react";
-import router from "next/router";
+import { useRouter } from 'next/navigation';
 
 const RegisterDialog = () => {
   const [username, setUsername] = useState("");
@@ -25,6 +25,8 @@ const RegisterDialog = () => {
   const [lastname, setLastname] = useState("");
   const [mail, setMail] = useState("");
   const [error, setError] = useState("");
+  const [opened, setOpened] = useState(true);
+  const router = useRouter();
 
   const handleRegister = async (
     username: string,
@@ -35,6 +37,10 @@ const RegisterDialog = () => {
     mail: string
     ) => {
 
+      if (username === "" || password === "" || password2 === "" || firstname === "" || lastname === "" || mail === "") {
+        setError("Please fill all fields");
+        return;
+      }
       if (password !== password2) {
       setError("Passwords do not match");
       return;
@@ -56,6 +62,7 @@ const RegisterDialog = () => {
             },
           },
         });
+        setOpened(false);
         router.push('/');
       } catch (e) {
         console.error('Error register standard:', e);
@@ -67,88 +74,90 @@ const RegisterDialog = () => {
       <DialogTrigger asChild>
         <Button variant="black">Register</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] max-w-72">
-        <DialogHeader>
-          <DialogTitle>Register</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              placeholder="foo_bar"
-              className="col-span-2"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="password" className="text-right">
-              Password
-            </Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="********"
-              className="col-span-2"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="password2" className="text-right">
-              Repeat Password
-            </Label>
-            <Input
-              id="password2"
-              type="password"
-              placeholder="********"
-              className="col-span-2"
-              onChange={(e) => setPassword2(e.target.value)}
-            />
-          </div>
-          <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="firstname" className="text-right">
-              Firstname
-            </Label>
-            <Input
-              id="firstname"
-              type="firstname"
-              placeholder="Elie"
-              className="col-span-2"
-              onChange={(e) => setFirstname(e.target.value)}
-            />
-          </div>
-          <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="lastname" className="text-right">
-                Lastname
+      { opened &&
+        <DialogContent className="sm:max-w-[425px] max-w-72">
+          <DialogHeader>
+            <DialogTitle>Register</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
               </Label>
               <Input
-                id="lastname"
-                type="lastname"
-                placeholder="Copter"
+                id="username"
+                placeholder="foo_bar"
                 className="col-span-2"
-                onChange={(e) => setLastname(e.target.value)}
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="mail" className="text-right">
-                E-Mail
+              <Label htmlFor="password" className="text-right">
+                Password
               </Label>
               <Input
-                id="mail"
-                type="mail"
-                placeholder="toto@gmail.com"
+                id="password"
+                type="password"
+                placeholder="********"
                 className="col-span-2"
-                onChange={(e) => setMail(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-        </div>
-        {error && <div className="text-red-500">{error}</div>}
-        <DialogFooter>
-        <Button type="submit" onClick={() => handleRegister(username, password, password2, firstname, lastname, mail)}>Register</Button>
-        </DialogFooter>
-      </DialogContent>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="password2" className="text-right">
+                Repeat Password
+              </Label>
+              <Input
+                id="password2"
+                type="password"
+                placeholder="********"
+                className="col-span-2"
+                onChange={(e) => setPassword2(e.target.value)}
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="firstname" className="text-right">
+                Firstname
+              </Label>
+              <Input
+                id="firstname"
+                type="firstname"
+                placeholder="Elie"
+                className="col-span-2"
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="lastname" className="text-right">
+                  Lastname
+                </Label>
+                <Input
+                  id="lastname"
+                  type="lastname"
+                  placeholder="Copter"
+                  className="col-span-2"
+                  onChange={(e) => setLastname(e.target.value)}
+                />
+              </div>
+              <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="mail" className="text-right">
+                  E-Mail
+                </Label>
+                <Input
+                  id="mail"
+                  type="mail"
+                  placeholder="toto@gmail.com"
+                  className="col-span-2"
+                  onChange={(e) => setMail(e.target.value)}
+                />
+              </div>
+          </div>
+          {error && <div className="text-red-500">{error}</div>}
+          <DialogFooter>
+          <Button type="submit" onClick={() => handleRegister(username, password, password2, firstname, lastname, mail)}>Register</Button>
+          </DialogFooter>
+        </DialogContent>
+      }
     </Dialog>
   )
 }

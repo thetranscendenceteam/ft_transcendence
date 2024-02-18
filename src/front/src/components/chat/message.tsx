524745ab6539c2da5599585660d6223d05d46944
Fargesia01 <slott@student.42lausanne.ch>
2024-02-18 22:15:46 +0100

    pp_work

--- src/front/src/components/chat/message.tsx
+++ src/front/src/components/chat/message.tsx
@@ -5,6 +5,7 @@ type Entity = {
   timestamp: string;
   message: string;
   username: string;
+  avatar?: string;
   link?: string;
 }
 
@@ -18,7 +19,7 @@ const Message: React.FC<Props> = ({ message, isMine }) => {
 
   return (
     <div className={messageClass}>
-      <img className="w-8 h-8 rounded-full" src="https://avatars.githubusercontent.com/u/11646882" alt="Profile pic" />
+      <img className="w-8 h-8 rounded-full" src={message.avatar ? message.avatar : "https://avatars.githubusercontent.com/u/11646882"} alt="Profile pic" />
       <div className="flex flex-col leading-1.5 p-4 ml-2 border-gray-200 bg-indigo-400 rounded-e-xl rounded-es-xl">
         <div className="flex items-center space-x-2 rtl:space-x-reverse">
           <span className="text-sm font-semibold text-white">{message.username}</span>

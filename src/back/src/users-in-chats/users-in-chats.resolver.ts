11917fe3d4f5b29a4978c7c452db87e918ae4f8e
Loic Dubouloz <lulutalu@gmail.com>
2024-02-18 14:59:15 +0100

    Fix my bad @Fargesia

--- src/back/src/users-in-chats/users-in-chats.resolver.ts
+++ src/back/src/users-in-chats/users-in-chats.resolver.ts
@@ -8,37 +8,36 @@ import { LIMIT_COMPOUND_SELECT } from 'sqlite3';
 
 @Resolver()
 export class UsersInChatsResolver {
-    constructor(private usersInChatsService: UsersInChatsService) { }
+	constructor(private usersInChatsService: UsersInChatsService) { }
 
-    @Query(returns => [UserChatInfo])
-    getUsersByIdChat(
-        @Args('chatId', { type: () => String, nullable: false }) chatId: string
-    ): Promise<UserChatInfo[]> {
-        console.log("getUsersByIdChat query with chatId : " + chatId);
-        return this.usersInChatsService.getAllUsersByChatId(chatId);
-    }
+	@Query(returns => [UserChatInfo])
+	getUsersByIdChat(
+  	@Args('chatId', { type: () => String, nullable: false }) chatId: string
+	): Promise<UserChatInfo[]> {
+    console.log("getUsersByIdChat query with chatId : " + chatId);
+    return this.usersInChatsService.getAllUsersByChatId(chatId);
+	}
 
-    @Query(returns => [ChatsInfoWithUser])
-    async getChatsByIdUser(
-        @Args('userId', { type: () => String, nullable: false }) userId: string
-    ): Promise<ChatsInfoWithUser[]> {
-        console.log("getChatsByIdUser query with userId : " + userId);
+	@Query(returns => [ChatsInfoWithUser])
+	async getChatsByIdUser(
+    @Args('userId', { type: () => String, nullable: false }) userId: string
+	): Promise<ChatsInfoWithUser[]> {
+    console.log("getChatsByIdUser query with userId : " + userId);
 		const chats = await this.usersInChatsService.getAllChatByUserId(userId);
 		for (const i of chats) {
-			if (i.isPrivate) {
+			if (i.isWhisper) {
 				const users = await this.usersInChatsService.getAllUsersByChatId(i.idChat);
 				const user = users.filter((u) => u.idUser != userId);
 				i.userInfo = user[0];
 			}
-
 		}
 		return chats;
-    }
+	}
 
 	@Query(returns => ChatUserInfo)
-    getInfoUserForIdChatAndIdUser(
-        @Args('InfoChatForUserInput') input: InfoChatForUserInput,
-    ): Promise<ChatUserInfo | null> {
-        return this.usersInChatsService.getInfoUser(input);
-    }
+	getInfoUserForIdChatAndIdUser(
+    @Args('InfoChatForUserInput') input: InfoChatForUserInput,
+	): Promise<ChatUserInfo | null> {
+    return this.usersInChatsService.getInfoUser(input);
+	}
 }

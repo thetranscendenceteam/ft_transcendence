2ef6c8eeb1538018654df2849ee8abbd69c7b4d5
Loic Dubouloz <lulutalu@gmail.com>
2024-02-18 13:23:01 +0100

    Added infos on all users inside previous query

--- src/back/src/users-in-chats/users-in-chats.resolver.ts
+++ src/back/src/users-in-chats/users-in-chats.resolver.ts
@@ -3,6 +3,7 @@ import { UsersInChatsService } from './users-in-chats.service';
 import { UserChatInfo } from './dto/UserChatInfo.entity';
 import { ChatUserInfo } from './dto/ChatUserInfo.entity';
 import { InfoChatForUserInput } from './dto/getInfoChatForUser.input';
+import { ChatsInfoWithUser } from './dto/ChatsInfoWithUser.entity';
 import { LIMIT_COMPOUND_SELECT } from 'sqlite3';
 
 @Resolver()
@@ -17,15 +18,24 @@ export class UsersInChatsResolver {
         return this.usersInChatsService.getAllUsersByChatId(chatId);
     }
 
-    @Query(returns => [ChatUserInfo])
-    getChatsByIdUser(
+    @Query(returns => [ChatsInfoWithUser])
+    async getChatsByIdUser(
         @Args('userId', { type: () => String, nullable: false }) userId: string
-    ): Promise<ChatUserInfo[]> {
+    ): Promise<ChatsInfoWithUser[]> {
         console.log("getChatsByIdUser query with userId : " + userId);
-        return this.usersInChatsService.getAllChatByUserId(userId);
+		const chats = await this.usersInChatsService.getAllChatByUserId(userId);
+		for (const i of chats) {
+			if (i.isPrivate) {
+				const users = await this.usersInChatsService.getAllUsersByChatId(i.idChat);
+				const user = users.filter((u) => u.idUser != userId);
+				i.userInfo = user[0];
+			}
+
+		}
+		return chats;
     }
 
-    @Query(returns => ChatUserInfo)
+	@Query(returns => ChatUserInfo)
     getInfoUserForIdChatAndIdUser(
         @Args('InfoChatForUserInput') input: InfoChatForUserInput,
     ): Promise<ChatUserInfo | null> {

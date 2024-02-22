cee94fb012b0e82b15b1b1fe7f750f94618048c4
Yoël Cornamusaz <iioel64@gmail.com>
2024-02-22 23:16:12 +0100

    Fix apollo errors

--- src/front/src/components/friendsNotifs/notifsButton.tsx
+++ src/front/src/components/friendsNotifs/notifsButton.tsx
@@ -1,14 +1,12 @@
 import React, { useContext, useEffect, useState, useRef, useCallback } from 'react';
 import { UserContext } from '../userProvider';
 import apolloClient from '../apolloclient';
-import { OnSubscriptionDataOptions, gql, useQuery, useSubscription } from "@apollo/client"
+import { OnDataOptions, gql, useSubscription } from "@apollo/client"
 
 type FriendsNotifs = {
   name: string,
-	userId: string,
+  userId: string,
 };
-
-const NEW_FRIENDREQUEST = 'newFriendRequest_';
 
 const NotificationsButton = () => {
   const [showNotifications, setShowNotifications] = useState(false);
@@ -21,12 +19,12 @@ const NotificationsButton = () => {
     try {
       const { data } = await apolloClient.query({
         query: gql`
-					query findPendingRequest($input: String!) {
-						findPendingRequest(userId: $input) {
-							userId
-							username
-						}
-					}
+          query findPendingRequest($input: String!) {
+            findPendingRequest(userId: $input) {
+              userId
+              username
+            }
+          }
         `,
         variables: {
           input: id,
@@ -39,33 +37,33 @@ const NotificationsButton = () => {
   };
 
   const NEW_FRIENDREQUEST_SUBSCRIPTION = gql`
-  subscription newPendingRequest($userId: String!) {
-    newPendingRequest(userId: $userId) {
-      userId
-      username
+    subscription newPendingRequest($userId: String!) {
+      newPendingRequest(userId: $userId) {
+        userId
+        username
+      }
     }
-  }
-`;
+  `;
 
   const refuse = async (userId: string) => {
     try {
       await apolloClient.mutate({
         mutation: gql`
-					mutation refusePending($acceptOrNot: Boolean!, $relationshipInput: RelationshipInput!) {
-						acceptOrRefusePending(acceptOrNot: $acceptOrNot, relationshipInput: $relationshipInput)
-					}
+          mutation refusePending($acceptOrNot: Boolean!, $relationshipInput: RelationshipInput!) {
+            acceptOrRefusePending(acceptOrNot: $acceptOrNot, relationshipInput: $relationshipInput)
+          }
         `,
         variables: {
           acceptOrNot: false,
-					relationshipInput: {
-						userId: user?.id,
-						targetId: userId
-					}
+          relationshipInput: {
+            userId: user?.id,
+            targetId: userId
+          }
         },
       });
-			if (user) {
-				fetchData(user.id);
-			}
+      if (user) {
+        fetchData(user.id);
+      }
     } catch (error) {
       return [];
     }
@@ -75,21 +73,21 @@ const NotificationsButton = () => {
     try {
       await apolloClient.mutate({
         mutation: gql`
-					mutation acceptPending($acceptOrNot: Boolean!, $relationshipInput: RelationshipInput!) {
-						acceptOrRefusePending(acceptOrNot: $acceptOrNot, relationshipInput: $relationshipInput)
-					}
+          mutation acceptPending($acceptOrNot: Boolean!, $relationshipInput: RelationshipInput!) {
+            acceptOrRefusePending(acceptOrNot: $acceptOrNot, relationshipInput: $relationshipInput)
+          }
         `,
         variables: {
           acceptOrNot: true,
-					relationshipInput: {
-						userId: user?.id,
-						targetId: userId
-					}
+          relationshipInput: {
+            userId: user?.id,
+            targetId: userId
+          }
         },
       });
-			if (user) {
-				fetchData(user.id);
-			}
+      if (user) {
+        fetchData(user.id);
+      }
     } catch (error) {
       return [];
     }
@@ -99,16 +97,16 @@ const NotificationsButton = () => {
     setShowNotifications(!showNotifications);
   };
 
-  const onMessage = useCallback((result: OnSubscriptionDataOptions) => {
-    if (!result.subscriptionData.data) return;
-    const lastMessage = result.subscriptionData.data.newPendingRequest;
+  const onMessage = useCallback((result: OnDataOptions) => {
+    if (!result.data.data) return;
+    const lastMessage = result.data.data.newPendingRequest;
     setFriendsNotifs((prevMessages) => [...prevMessages, lastMessage]);
   }, []);
 
   useSubscription(NEW_FRIENDREQUEST_SUBSCRIPTION, {
     variables: { "userId": user?.id },
     fetchPolicy: "no-cache",
-    onSubscriptionData: onMessage
+    onData: onMessage
   });
 
   useEffect(() => {
@@ -141,54 +139,54 @@ const NotificationsButton = () => {
   }, []);
 
   return (
-		<div style={{ position: 'relative', marginLeft: '10px', marginRight: '10px' }}>
-			{friendsNotifs.length > 0 ? (
-				<button ref={buttonRef} onClick={toggleNotifications}>
-					🟢
-				</button>
-			) : (
-				<button ref={buttonRef} onClick={toggleNotifications}>
-					⚪
-				</button>
-			)}
-			{showNotifications && (
-				<div
-					ref={notificationsRef}
-					style={{
-						position: 'absolute',
-						top: buttonRef.current?.offsetTop ?? 0,
-						right: 'calc(100% + 10px)',
-						backgroundColor: 'white',
-						border: '1px solid #24203E',
-						borderRadius: '5px',
-						padding: '5px',
-						background: '#060114',
-						color: 'white',
-					}}
-				>
-					{friendsNotifs.length === 0 ? (
-						<p style={{ fontSize: '14px', whiteSpace: 'nowrap' }}>No pending friends requests</p>
-					) : (
-						<ul>
-							{friendsNotifs.map((notification: any, index: any) => (
-								<div key={index} style={{ marginLeft: '5px', marginRight: '5px' }}>
-									<li style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-										{notification.username}
-										<button style={{ marginLeft: '5px' }} onClick={async () => await refuse(notification.userId)}>
-											❌
-										</button>
-										<button style={{ marginLeft: '5px' }} onClick={async () => await accept(notification.userId)}>
-											✅
-										</button>
-									</li>
-								</div>
-							))}
-						</ul>
-					)}
-				</div>
-			)}
-		</div>
-	);
+    <div style={{ position: 'relative', marginLeft: '10px', marginRight: '10px' }}>
+      {friendsNotifs.length > 0 ? (
+	<button ref={buttonRef} onClick={toggleNotifications}>
+  	  🟢
+  	</button>
+      ) : (
+  	<button ref={buttonRef} onClick={toggleNotifications}>
+  	  ⚪
+  	</button>
+      )}
+      {showNotifications && (
+	<div
+    	  ref={notificationsRef}
+    	  style={{
+    	    position: 'absolute',
+    	    top: buttonRef.current?.offsetTop ?? 0,
+    	    right: 'calc(100% + 10px)',
+    	    backgroundColor: 'white',
+    	    border: '1px solid #24203E',
+    	    borderRadius: '5px',
+    	    padding: '5px',
+    	    background: '#060114',
+    	    color: 'white',
+    	  }}
+    	>
+    	  {friendsNotifs.length === 0 ? (
+    	    <p style={{ fontSize: '14px', whiteSpace: 'nowrap' }}>No pending friends requests</p>
+    	  ) : (
+    	    <ul>
+    	      {friendsNotifs.map((notification: any, index: any) => (
+    	        <div key={index} style={{ marginLeft: '5px', marginRight: '5px' }}>
+    	          <li style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+    	            {notification.username}
+    	            <button style={{ marginLeft: '5px' }} onClick={async () => await refuse(notification.userId)}>
+    	              ❌
+    	            </button>
+    	            <button style={{ marginLeft: '5px' }} onClick={async () => await accept(notification.userId)}>
+    	              ✅
+    	            </button>
+    	          </li>
+    	        </div>
+    	      ))}
+    	    </ul>
+    	  )}
+    	</div>
+      )}
+    </div>
+  );
 };
 
 export default NotificationsButton;
